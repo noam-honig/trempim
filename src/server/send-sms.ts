e272206aeb847d2aa54c3b66602922e21495f2cb
Noam Honig <noam.honig@gmail.com>
2023-10-16 22:37:04 +0300

    fixed sms send to use other provider

--- src/server/send-sms.ts
+++ src/server/send-sms.ts
@@ -1,12 +1,15 @@
 export async function sendSms(phone: string, message: string): Promise<any> {
-  if (process.env['disable_sms']) {
+  if (process.env['disable_sms'] && false) {
     console.log({ phone, message })
     return
   }
   const fetch = await import('node-fetch')
+  const FormData = await import('form-data')
   let un = process.env['SMS_UN']
   let pw = process.env['SMS_PW']
   let accid = process.env['SMS_ACCID']
+  const inforuToken = process.env['INFORU_SMS_TOKEN']
+  let useGlobalSms = !inforuToken
   var from = 'Hagai'
   if (!accid) return 'חשבון SMS לא הוגדר'
   phone = phone.replace(/\D/g, '')
@@ -28,69 +31,114 @@ export async function sendSms(phone: string, message: string): Promise<any> {
   const send = async () => {
     try {
       {
-        message = message
-          .replace(/&/g, '&amp;')
-          .replace(/</g, '&lt;')
-          .replace(/>/g, '&gt;')
-          .replace(/"/g, '&quot;')
-          .replace(/'/g, '&apos;')
+        if (useGlobalSms) {
+          message = message
+            .replace(/&/g, '&amp;')
+            .replace(/</g, '&lt;')
+            .replace(/>/g, '&gt;')
+            .replace(/"/g, '&quot;')
+            .replace(/'/g, '&apos;')
 
-        let data =
-          '<?xml version="1.0" encoding="utf-8"?>' +
-          '<soap12:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soap12="http://www.w3.org/2003/05/soap-envelope">' +
-          '<soap12:Body>' +
-          '<sendSmsToRecipients xmlns="apiItnewsletter">' +
-          '<un>' +
-          un +
-          '</un>' +
-          '<pw>' +
-          pw +
-          '</pw>' +
-          '<accid>' +
-          accid +
-          '</accid>' +
-          '<sysPW>' +
-          'itnewslettrSMS' +
-          '</sysPW>' +
-          '<t>' +
-          date +
-          '</t>' +
-          '<txtUserCellular>' +
-          from +
-          '</txtUserCellular>' +
-          '<destination>' +
-          phone +
-          '</destination>' +
-          '<txtSMSmessage>' +
-          message +
-          '</txtSMSmessage>' +
-          '<dteToDeliver></dteToDeliver>' +
-          '<txtAddInf>jsnodetest</txtAddInf>' +
-          '</sendSmsToRecipients>' +
-          '</soap12:Body>' +
-          '</soap12:Envelope>'
-        let h = new fetch.Headers()
-        h.append('Content-Type', 'text/xml; charset=utf-8')
-        h.append('SOAPAction', 'apiItnewsletter/sendSmsToRecipients')
-        let r = await fetch.default(
-          'https://sapi.itnewsletter.co.il/webservices/webservicesms.asmx',
-          {
-            method: 'POST',
-            headers: h,
-            body: data,
+          let data =
+            '<?xml version="1.0" encoding="utf-8"?>' +
+            '<soap12:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soap12="http://www.w3.org/2003/05/soap-envelope">' +
+            '<soap12:Body>' +
+            '<sendSmsToRecipients xmlns="apiItnewsletter">' +
+            '<un>' +
+            un +
+            '</un>' +
+            '<pw>' +
+            pw +
+            '</pw>' +
+            '<accid>' +
+            accid +
+            '</accid>' +
+            '<sysPW>' +
+            'itnewslettrSMS' +
+            '</sysPW>' +
+            '<t>' +
+            date +
+            '</t>' +
+            '<txtUserCellular>' +
+            from +
+            '</txtUserCellular>' +
+            '<destination>' +
+            phone +
+            '</destination>' +
+            '<txtSMSmessage>' +
+            message +
+            '</txtSMSmessage>' +
+            '<dteToDeliver></dteToDeliver>' +
+            '<txtAddInf>jsnodetest</txtAddInf>' +
+            '</sendSmsToRecipients>' +
+            '</soap12:Body>' +
+            '</soap12:Envelope>'
+          let h = new fetch.Headers()
+          h.append('Content-Type', 'text/xml; charset=utf-8')
+          h.append('SOAPAction', 'apiItnewsletter/sendSmsToRecipients')
+          let r = await fetch.default(
+            'https://sapi.itnewsletter.co.il/webservices/webservicesms.asmx',
+            {
+              method: 'POST',
+              headers: h,
+              body: data,
+            }
+          )
+
+          let res = await r.text()
+          let orig = res
+          let t = '<sendSmsToRecipientsResult>'
+          let i = res.indexOf(t)
+          if (i >= 0) {
+            res = res.substring(i + t.length)
+            res = res.substring(0, res.indexOf('<'))
           }
-        )
+          console.log('sms response: - ' + res)
+          return res
+        } else {
+          const data = `
+<Inforu>
+<User>
+<Username>${process.env['INFORU_SMS_USER']}</Username>
+<ApiToken>${inforuToken}</ApiToken>
+</User>
+<Content Type="sms">
+<Message>${message}</Message>
+</Content>
+<Recipients>
+<PhoneNumber>${phone}</PhoneNumber>
+</Recipients>
+<Settings>
+<Sender>${from}</Sender>
+</Settings>
+</Inforu>`
+          let h = new fetch.Headers()
+          var formData = new FormData.default()
 
-        let res = await r.text()
+          formData.append('InforuXML', data)
 
-        let t = '<sendSmsToRecipientsResult>'
-        let i = res.indexOf(t)
-        if (i >= 0) {
-          res = res.substring(i + t.length)
-          res = res.substring(0, res.indexOf('<'))
+          let r = await fetch.default(
+            'https://api.inforu.co.il/SendMessageXml.ashx',
+            {
+              method: 'POST',
+
+              headers: formData.getHeaders(),
+
+              body: formData,
+            }
+          )
+
+          let res = await r.text()
+          let orig = res
+          let t = '<sendSmsToRecipientsResult>'
+          let i = res.indexOf(t)
+          if (i >= 0) {
+            res = res.substring(i + t.length)
+            res = res.substring(0, res.indexOf('<'))
+          }
+          console.log('sms response:' + res)
+          return res
         }
-        console.log('sms response:' + res)
-        return res
       }
     } catch (err: any) {
       console.error('sms error ', err)
