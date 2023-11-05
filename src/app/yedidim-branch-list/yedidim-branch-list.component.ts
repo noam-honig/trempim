93cd0185dda76f090830fd2f97866525b7e43bf9
Noam Honig <noam.honig@gmail.com>
2023-11-05 17:19:27 +0200

    added מחוז

--- src/app/yedidim-branch-list/yedidim-branch-list.component.ts
+++ src/app/yedidim-branch-list/yedidim-branch-list.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core'
 import { Task } from '../events/tasks'
 import copy from 'copy-to-clipboard'
 import { UIToolsService } from '../common/UIToolsService'
+import { getRegion } from '../common/address-input/google-api-helpers'
 
 @Component({
   selector: 'app-yedidim-branch-list',
@@ -14,8 +15,10 @@ export class YedidimBranchListComponent implements OnInit {
     tasks: Task[]
     category?: string
   }
-  districts: { name: string; branches: { name: string; message: string }[] }[] =
-    []
+  regions: {
+    name: string
+    districts: { name: string; branches: { name: string; message: string }[] }[]
+  }[] = []
 
   copy(what: string) {
     copy(what)
@@ -23,19 +26,21 @@ export class YedidimBranchListComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    let districts = new Map<
-      string | undefined,
-      Map<string | undefined, Task[]>
-    >()
+    let regions = new Map<string, Map<string, Map<string, Task[]>>>()
     for (const t of this.args.tasks) {
-      let items = districts.get(t.addressApiResult?.district)
-      if (!items) {
-        districts.set(t.addressApiResult?.district, (items = new Map()))
+      let districts = regions.get(getRegion(t.addressApiResult))
+      if (!districts) {
+        regions.set(getRegion(t.addressApiResult), (districts = new Map()))
       }
 
-      let x = items.get(t.addressApiResult?.branch)
+      let items = districts.get(t.addressApiResult?.district || '')
+      if (!items) {
+        districts.set(t.addressApiResult?.district || '', (items = new Map()))
+      }
+
+      let x = items.get(t.addressApiResult?.branch || '')
       if (!x) {
-        items.set(t.addressApiResult?.branch, (x = [t]))
+        items.set(t.addressApiResult?.branch || '', (x = [t]))
       } else x.push(t)
     }
 
@@ -43,26 +48,33 @@ export class YedidimBranchListComponent implements OnInit {
       x.sort((a, b) => (a || '').localeCompare(b || ''))
       return x
     }
-    for (const d of sortByName([...districts.keys()])) {
-      let items = districts.get(d)!
-      let branches: { name: string; message: string }[] = []
-      this.districts.push({ name: d || '', branches })
-      for (const x of sortByName([...items.keys()])) {
-        let tasks = items.get(x)
-        let message = 'קריאות פתוחות לסניף ' + x
 
-        if (this.args.category) {
-          message += ` (${this.args.category})`
+    for (const r of sortByName([...regions.keys()])) {
+      const districts = regions.get(r!)!
+
+      this.regions.push({ name: r || '', districts: [] })
+      let region = this.regions[this.regions.length - 1]
+      for (const d of sortByName([...districts.keys()])) {
+        let items = districts.get(d!)!
+        let branches: { name: string; message: string }[] = []
+        region.districts.push({ name: d || '', branches })
+        for (const x of sortByName([...items.keys()])) {
+          let tasks = items.get(x!)
+          let message = 'קריאות פתוחות לסניף ' + x
+
+          if (this.args.category) {
+            message += ` (${this.args.category})`
+          }
+          let t = new Date()
+          message += ` - שעה ${t.getHours()}:${(
+            t.getMinutes() / 10
+          ).toFixed()}0 (${tasks?.length} קריאות): \n`
+          message += tasks
+            ?.map((e) => '* ' + e.getShortDescription() + '\n' + e.getLink())
+            .join('\n\n')
+
+          branches.push({ name: (x || '') + ' - ' + tasks?.length, message })
         }
-        let t = new Date()
-        message += ` - שעה ${t.getHours()}:${(
-          t.getMinutes() / 10
-        ).toFixed()}0 (${tasks?.length} קריאות): \n`
-        message += tasks
-          ?.map((e) => '* ' + e.getShortDescription() + '\n' + e.getLink())
-          .join('\n\n')
-
-        branches.push({ name: (x || '') + ' - ' + tasks?.length, message })
       }
     }
   }
